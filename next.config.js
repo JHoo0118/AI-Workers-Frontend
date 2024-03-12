2b7633c0140547b48c90c7f735c14ee4d88de80d
JHoo0118 <kik3078@gmail.com>
2024-03-12 23:04:48 +0900

    change next.config.js

--- next.config.js
+++ next.config.js
@@ -20,21 +20,21 @@ const nextConfig = {
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/api/:path*"
-            : "/api/",
+            : `${process.env.BASE_API_URL}/api/`,
       },
       {
         source: "/docs",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/docs"
-            : "/api/docs",
+            : `${process.env.BASE_API_URL}/api/docs`,
       },
       {
         source: "/openapi.json",
         destination:
           process.env.NODE_ENV === "development"
             ? "http://127.0.0.1:8000/openapi.json"
-            : "/api/openapi.json",
+            : `${process.env.BASE_API_URL}/api/openapi.json`,
       },
     ];
   },
