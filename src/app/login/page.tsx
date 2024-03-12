1dac1f3779affa0df92bbc0387752a5a36bf908e
JHoo0118 <kik3078@gmail.com>
2024-03-12 23:40:27 +0900

    add suspense

--- src/app/login/page.tsx
+++ src/app/login/page.tsx
@@ -13,21 +13,19 @@ import LoadingButton from "@/components/ui/loading-button";
 import { FORWARD_URL } from "@/const/const";
 import { useAuth } from "@/context/AuthContext";
 import { LoginSchema, loginSchema } from "@/lib/validation/loginSchema";
-import { googleLogin } from "@/service/auth/auth";
 import { zodResolver } from "@hookform/resolvers/zod";
 import Link from "next/link";
-import { useRouter, useSearchParams } from "next/navigation";
+import { useSearchParams } from "next/navigation";
+import { Suspense } from "react";
 import { useForm } from "react-hook-form";
 
-const LoginPage = () => {
+const LoginPageContent = () => {
   const searchParams = useSearchParams();
   const forwardUrl = searchParams.get(FORWARD_URL);
-  const router = useRouter();
   const { login } = useAuth();
   const form = useForm<LoginSchema>({
     resolver: zodResolver(loginSchema),
   });
-
   const onSubmit = async (data: LoginSchema) => {
     await login(
       data,
@@ -39,41 +37,35 @@ const LoginPage = () => {
     );
   };
 
-  async function onClickGoogleLogin() {
-    const { url } = await googleLogin();
-    router.push(url);
-  }
-
+  // async function onClickGoogleLogin() {
+  //   const { url } = await googleLogin();
+  //   router.push(url);
+  // }
   return (
-    <div className="flex h-full flex-col items-center justify-center">
-      <div className="w-full max-w-md space-y-6 rounded-md bg-card p-8 shadow-md dark:bg-secondary">
-        <h1 className="text-3xl">AI Workers 로그인</h1>
-        <Form {...form}>
-          <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
-            <FormItem>
-              <FormLabel>이메일</FormLabel>
-              <FormControl>
-                <Input type="email" {...form.register("email")} />
-              </FormControl>
-              <FormMessage>{form.formState.errors.email?.message}</FormMessage>
-            </FormItem>
-            <FormItem>
-              <FormLabel>비밀번호</FormLabel>
-              <FormControl>
-                <Input type="password" {...form.register("password")} />
-              </FormControl>
-              <FormMessage>
-                {form.formState.errors.password?.message}
-              </FormMessage>
-            </FormItem>
-            <LoadingButton
-              className="w-full"
-              type="submit"
-              loading={form.formState.isSubmitting}
-            >
-              로그인
-            </LoadingButton>
-            {/* <div className="mt-4 flex items-center justify-between">
+    <Form {...form}>
+      <form onSubmit={form.handleSubmit(onSubmit)} className="space-y-6">
+        <FormItem>
+          <FormLabel>이메일</FormLabel>
+          <FormControl>
+            <Input type="email" {...form.register("email")} />
+          </FormControl>
+          <FormMessage>{form.formState.errors.email?.message}</FormMessage>
+        </FormItem>
+        <FormItem>
+          <FormLabel>비밀번호</FormLabel>
+          <FormControl>
+            <Input type="password" {...form.register("password")} />
+          </FormControl>
+          <FormMessage>{form.formState.errors.password?.message}</FormMessage>
+        </FormItem>
+        <LoadingButton
+          className="w-full"
+          type="submit"
+          loading={form.formState.isSubmitting}
+        >
+          로그인
+        </LoadingButton>
+        {/* <div className="mt-4 flex items-center justify-between">
               <div className="h-[1px] w-full bg-slate-200 dark:bg-slate-700"></div>
               <span className="min-w-12 text-center text-xs uppercase">
                 또는
@@ -96,8 +88,19 @@ const LoginPage = () => {
               />
               <span>구글로 로그인</span>
             </Button> */}
-          </form>
-        </Form>
+      </form>
+    </Form>
+  );
+};
+
+const LoginPage = () => {
+  return (
+    <div className="flex h-full flex-col items-center justify-center">
+      <div className="w-full max-w-md space-y-6 rounded-md bg-card p-8 shadow-md dark:bg-secondary">
+        <h1 className="text-3xl">AI Workers 로그인</h1>
+        <Suspense fallback={<div></div>}>
+          <LoginPageContent />
+        </Suspense>
         <h3>
           아직 계정이 없으신가요?{" "}
           <span className="cursor-pointer text-teal-400 underline">
