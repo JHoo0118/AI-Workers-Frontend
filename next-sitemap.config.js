6e6e99ec5a8e3950b030c8246a12d415dc95f905
JHoo0118 <kik3078@gmail.com>
2024-04-14 17:21:23 +0900

    sitemap.xml

---
+++ next-sitemap.config.js
@@ -0,0 +1,26 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: "https://smartaiworkers.com",
+  generateRobotsTxt: true,
+  changefreq: "daily",
+  priority: 0.7,
+  alternateRefs: [
+    {
+      href: "https://smartaiworkers.com/ko",
+      hreflang: "ko",
+    },
+    {
+      href: "https://smartaiworkers.com/en",
+      hreflang: "en",
+    },
+  ],
+  transform: async (config, path) => {
+    return {
+      loc: path,
+      changefreq: config.changefreq,
+      priority: config.priority,
+      lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
+      alternateRefs: config.alternateRefs ?? [],
+    };
+  },
+};
