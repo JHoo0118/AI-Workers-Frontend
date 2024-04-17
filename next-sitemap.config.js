6e287b9f392a192adb3bda53d6b7a5cbb86be169
JHoo0118 <kik3078@gmail.com>
2024-04-17 23:17:34 +0900

    sitemap 수정

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -4,16 +4,6 @@ module.exports = {
   generateRobotsTxt: true,
   changefreq: "daily",
   priority: 0.7,
-  alternateRefs: [
-    {
-      href: "https://smartaiworkers.com/ko",
-      hreflang: "ko",
-    },
-    {
-      href: "https://smartaiworkers.com/en",
-      hreflang: "en",
-    },
-  ],
   transform: async (config, path) => {
     return {
       loc: path,
@@ -21,7 +11,6 @@ module.exports = {
       priority: config.priority,
       lastmod: config.autoLastmod ? new Date().toISOString() : undefined,
       alternateRefs: config.alternateRefs ?? [],
-      exclude: ["/ko/account/*", "/en/account/*"],
     };
   },
 };
